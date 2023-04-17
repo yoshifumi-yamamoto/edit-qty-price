a9313f17ecafd31c69db63a417dd130619c9a79d
yoshifumi-yamamoto <toyotama.arank.acekiller@gmail.com>
2023-04-17 13:24:08 +0900

    以下の更新 ・在庫がある商品のqtyを1にする ・在庫がある商品の最新仕入れ価格を商品管理に入力 ・ループ内でのAPI呼び出しを最適化

--- index.js
+++ index.js
@@ -45,6 +45,8 @@ function sendForm(formObject) {
   // ebayURL列全取得
   const ebayURLs = urlGetSheet.getSheetByName("出品 年月").getRange(2,12,20000,1).getValues();
 
+  // 仕入価格列全取得
+  var supplierPricies = urlGetSheet.getSheetByName("出品 年月").getRange(2,6,20000,1).getValues();
 
 
   // 二次元配列を一次元配列に変換
@@ -64,39 +66,50 @@ function sendForm(formObject) {
   var addValues = []
   // 売り切れ判定
   const soldOutsMsgs = ['', '売り切れました']
+  // 設定シート
+  const Settings = ss.getSheetByName(SETTING_SHEET_NAME)
+  // URLを削除するかどうか
+  const deleteFlg = Settings.getRange('A2').getValue() === "ON"
 
   // １行目は項目名なのでsliceで排除
   values.slice(1).map(function (value) {
-    console.log(value)
-    console.log('value')
-    console.log('https://jp.mercari.com/item/m99925351601')
     // 在庫状況が空だったら売り切れ判定
-    const isSoldOut =  soldOutsMsgs.indexOf(value[1]) !== -1
-    if(isSoldOut){
-      // 売り切れだったら配列にurlを追加
-      soldOuts.push(value[2])
-    }
-  })
+    const buyNowBtnMsg = value[1]
+    const isSoldOut =  soldOutsMsgs.indexOf(buyNowBtnMsg) !== -1
+    // 仕入れ先URL
+    const supplierURL = value[2]
 
-  const Settings = ss.getSheetByName(SETTING_SHEET_NAME)
-  const deleteFlg = Settings.getRange('A2').getValue() === "ON"
-
-  soldOuts.map(function(soldOut) {
     // 仕入れ先と同じ行のitemNumberを取得する
-    const urlRow = formattedSuppliers.indexOf(soldOut)
-    console.log(soldOut)
-    const itemNumber =  formattedEbayURLs[urlRow] ? formattedEbayURLs[urlRow].replace('https://www.ebay.com/itm/', '') : formattedSuppliers[urlRow]
-
-    // Action(Revise = 変更), itemNumber, qty
-    addValues.push(['Revise', itemNumber, 0])
-    if(deleteFlg){
-      //   仕入れ先url 削除                                  ヘッダーの2行分ずらす
-      urlGetSheet.getSheetByName("出品 年月").getRange(urlRow + 2, 5).setValue('');
+    const itemRow = formattedSuppliers.indexOf(supplierURL)
+    const itemNumber =  formattedEbayURLs[itemRow] ? formattedEbayURLs[itemRow].replace('https://www.ebay.com/itm/', '') : formattedSuppliers[itemRow]
+    const latestPrice = value[3]
+    if(isSoldOut){
+      // 売り切れだったらqtyを0
+      // Action(Revise = 変更), itemNumber, qty
+      addValues.push(['Revise', itemNumber, 0])
+      // 仕入れ先を配列から削除
+      suppliers[itemRow][0] = ''
+    }else{
+      // 最新の仕入価格を配列に上書き
+      supplierPricies[itemRow][0] = latestPrice
+      addValues.push(['Revise', itemNumber, 1])
     }
-    
   })
+  // 仕入れ価格の更新
+  urlGetSheet.getSheetByName("出品 年月").getRange(2,6,20000,1).setValues(supplierPricies)
+  // flagがONなら仕入れ先URLを商品管理表から削除
+  if(deleteFlg){
+    // 仕入れ先url更新
+    urlGetSheet.getSheetByName("出品 年月").getRange(2,5,20000,1).setValues(suppliers);
+  }
   
   // 既存レコードをクリアし、CSVのレコードを貼り付け
   // clearRecords(RC_ROW, RC_COL, sheet);
   sheet.getRange(RC_ROW, RC_COL, addValues.length, addValues[0].length).setValues(addValues);
+}
+
+function clearSheet1() {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET_NAME);
+  const range = sheet.getRange(2, 1, sheet.getLastRow() - 1, 3);
+  range.clearContent();
 }
